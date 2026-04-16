4a54caca5ea677ebb10c215bb71dc7bf2c152fe2
ronylonts <rolandlontsie604@mail.com>
2026-04-16 18:42:58 +0200

    Evaluation Jour 2 - Roland Lontsie - corrections finales

--- symfoconnect/src/Controller/SecurityController.php
+++ symfoconnect/src/Controller/SecurityController.php
@@ -7,18 +7,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityControlleController extends AbstractController
+class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('securitycontrolle/login.html.twig', [
+        return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
